a7f96ed6f8399d4142ea3ddca53d2b4248ebd36d
Jérémy Guilpin <jeremy-guilpin@users.noreply.github.com>
2018-06-16 15:31:24 +0200

    Update geoloc.js

--- geoloc.js
+++ geoloc.js
@@ -38,6 +38,23 @@ function maPosition(position) {
 		});
 	}
 
+function showErrorGeoLoc(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.error("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.error("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.error("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.error("An unknown error occurred.");
+            break;
+    }
+}
+
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(maPosition, showErrorGeoloc, {
 			enableHighAccuracy: true, // On demande à l'appareil de rechercher une position la plus précise possible
